f72d661ab381def90f6cb54517baf82d1af43eb0
threedaysraiin <a-lubakova29@mail.ru>
2023-12-08 23:14:41 +0300

    класс игрока

--- player.py
+++ player.py
@@ -1,25 +1,17 @@
 from abc import ABC, abstractmethod
-from card import Cards
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-
-
 class Player(ABC):
     def __init__(self):
         print('Player init')
 
     @abstractmethod
-    def choose_card(self):
+    def choose_card(self, card, win_card):
         pass
-
 class HumanPlayer(Player):
     def __init__(self, name):
         super().__init__()
         self.name = name
         self.score = 0
-    def choose_card(self, cards, card, win_card):
+    def choose_card(self, card, win_card):
         if card == win_card:
             self.score += 1
             return True
